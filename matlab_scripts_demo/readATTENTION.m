acfe54d0ea859a9ac1ab3a535f8720a8e44902df
ikeblaster <ike@thez.info>
2014-03-23 17:52:07 +0100

    Ukázkové skripty pro MATLAB

---
+++ matlab_scripts_demo/readATTENTION.m
@@ -0,0 +1,98 @@
+function readRAW
+%run this function to connect and plot raw EEG data
+%make sure to change portnum1 to the appropriate COM port
+
+% src: http://www.youtube.com/watch?v=8mdw7StdRUU
+
+clear all
+close all
+
+data = zeros(1,256);    %preallocate buffer
+
+portnum1 = 5;   %COM Port #
+comPortName1 = sprintf('\\\\.\\COM%d', portnum1);
+
+
+% Baud rate for use with TG_Connect() and TG_SetBaudrate().
+TG_BAUD_57600 =      57600;
+
+
+% Data format for use with TG_Connect() and TG_SetDataFormat().
+TG_STREAM_PACKETS =     0;
+
+
+% Data type that can be requested from TG_GetValue().
+TG_DATA_RAW =         4;
+
+%load thinkgear dll
+loadlibrary('Thinkgear.dll');
+fprintf('Thinkgear.dll loaded\n');
+
+%get dll version
+dllVersion = calllib('Thinkgear', 'TG_GetDriverVersion');
+fprintf('ThinkGear DLL version: %d\n', dllVersion );
+
+
+%%
+% Get a connection ID handle to ThinkGear
+connectionId1 = calllib('Thinkgear', 'TG_GetNewConnectionId');
+if ( connectionId1 < 0 )
+    error( sprintf( 'ERROR: TG_GetNewConnectionId() returned %d.\n', connectionId1 ) );
+end;
+
+% Set/open stream (raw bytes) log file for connection
+errCode = calllib('Thinkgear', 'TG_SetStreamLog', connectionId1, 'streamLog.txt' );
+if( errCode < 0 )
+    error( sprintf( 'ERROR: TG_SetStreamLog() returned %d.\n', errCode ) );
+end;
+
+% Set/open data (ThinkGear values) log file for connection
+errCode = calllib('Thinkgear', 'TG_SetDataLog', connectionId1, 'dataLog.txt' );
+if( errCode < 0 )
+    error( sprintf( 'ERROR: TG_SetDataLog() returned %d.\n', errCode ) );
+end;
+
+% Attempt to connect the connection ID handle to serial port "COM3"
+errCode = calllib('Thinkgear', 'TG_Connect',  connectionId1,comPortName1,TG_BAUD_57600,TG_STREAM_PACKETS );
+if ( errCode < 0 )
+    error( sprintf( 'ERROR: TG_Connect() returned %d.\n', errCode ) );
+end
+
+fprintf( 'Connected.  Reading Packets...\n' );
+
+
+
+
+%%
+%record data
+
+j = 0;
+i = 0;
+while (i < 10240)   %loop for 20 seconds
+    if (calllib('Thinkgear','TG_ReadPackets',connectionId1,1) == 1)   %if a packet was read...
+        
+        if (calllib('Thinkgear','TG_GetValueStatus',connectionId1,TG_DATA_ATTENTION) ~= 0)   %if RAW has been updated 
+            j = j + 1;
+            i = i + 1;
+            data(j) = calllib('Thinkgear','TG_GetValue',connectionId1,TG_DATA_ATTENTION);
+            disp(data(j));
+            plot(data)
+            axis([0 1000 0 200])
+            drawnow;
+        end
+    end
+     
+    
+end
+
+
+
+
+
+%disconnect             
+calllib('Thinkgear', 'TG_FreeConnection', connectionId1 );
+
+
+
+
+
